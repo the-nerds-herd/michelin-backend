d6564a6f6bb0fb3a745f769384de93a6e482f7fa
DiannaNguyen <diannavnguyen@gmail.com>
2021-03-29 14:27:32 -0500

    Made connection.js

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const mongoURI =
+	process.env.NODE_ENV === 'production'
+		? process.env.DB_URL
+		: 'mongodb://localhost/michelinrestaurants';
+
+mongoose
+	.connect(mongoURI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	})
+	.then((instance) =>
+		console.log(`Connected to db: ${instance.connections[0].name}`)
+	)
+	.catch((error) => console.log('Connection failed!', error));
+
+module.exports = mongoose;
