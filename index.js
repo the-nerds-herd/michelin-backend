71c883e29354a319bfd329940f5c30375ebc27ff
DiannaNguyen <diannavnguyen@gmail.com>
2021-03-29 14:40:14 -0500

    Updated index.js - controllers

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const restaurantController = require('./controllers/restaurantController');
+const reviewController = require('./controllers/reviewController');
+const userController = require('./controllers/userController');
+
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Controller(s)
+app.use('/restaurants', restaurantController);
+app.use('/restaurants/reviews', reviewController);
+app.use('/restaurants/users', userController);
+
 
 const port = process.env.PORT || 8800;
 app.listen(port, () => {
