50a733322dc0f3059cbe0aaabdeecaba1735ee0e
DiannaNguyen <diannavnguyen@gmail.com>
2021-03-29 12:47:56 -0500

    Updated package.json, reviewed .gitignore, initialized index.js - imported express, cors, middleware, and added local port

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Controller(s)
+
+
+const port = process.env.PORT || 8800;
+app.listen(port, () => {
+	console.log('⭐ connected to port ' + port);
+});
