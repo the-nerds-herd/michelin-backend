04093dc0e081d4a3acac329f0378e61a4f47d6ea
jjcrab <lily_lijj@hotmail.com>
2021-04-02 11:53:01 -0600

    add signup router

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 router.get('/', (req, res, next) => {
 	User.find({})
@@ -9,10 +9,17 @@ router.get('/', (req, res, next) => {
 		.catch(next);
 });
 
-router.post('/', (req, res, next) => {
-	User.create(req.body)
-		.then((user) => res.json(user))
+//Sign up
+router.post('/signup', (req, res, next) => {
+	bcrypt
+		.hash(req.body.password, 10)
+		.then((hash) => ({ name: req.body.name, password: hash }))
+		.then((user) => User.create(user))
+		.then((user) => res.status(201).json(user))
 		.catch(next);
 });
+
 module.exports = router;
 
+//Sign in
+router.post('/signin', (req, res, next) => {});
--- models/User.js
+++ models/User.js
@@ -1,6 +1,27 @@
 const mongoose = require('../db/connection');
-const UserSchema = new mongoose.Schema({
-	name: { type: String, required: true },
-});
+const UserSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			retuired: true,
+		},
+	},
+	{
+		timestamps: true,
+		toJSON: {
+			virtuals: true,
+			transform: (_doc, ret) => {
+				ret.isCool = true;
+				delete ret.password;
+				return ret;
+			},
+		},
+	}
+);
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
